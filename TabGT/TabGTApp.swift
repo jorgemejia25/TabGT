d5f438d1bea528ebdb989583f138d4a3671e8f02
Jorge Mejía <jorge.mejia@profitsolv.com>
2026-05-30 16:50:30 -0600

    Initial Commit

---
+++ TabGT/TabGTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabGTApp.swift
+//  TabGT
+//
+//  Created by Jorge Andrés Mejía on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TabGTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TabGTTests/TabGTTests.swift
@@ -0,0 +1,18 @@
+//
+//  TabGTTests.swift
+//  TabGTTests
+//
+//  Created by Jorge Andrés Mejía on 30/05/26.
+//
+
+import Testing
+
+struct TabGTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
